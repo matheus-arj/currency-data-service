3632fb6d3c148cc5d094badfc4e330ef71c44a2d
matheus-arj <matheus.arj@hotmail.com>
2025-03-18 20:48:20 -0300

    feat: added promise any and adjusted error return

--- src/controllers/currency.controller.ts
+++ src/controllers/currency.controller.ts
@@ -3,17 +3,17 @@ import { CurrencyService } from '../services/currency.service';
 
 @Controller('currency')
 export class CurrencyController {
-    private readonly logger = new Logger(CurrencyController.name);
+  private readonly logger = new Logger(CurrencyController.name);
 
-    constructor(private readonly currencyService: CurrencyService) {}
+  constructor(private readonly currencyService: CurrencyService) {}
 
-    @Get('exchange-rates')
-    async getCurrency() {
-        try {
-            return await this.currencyService.getCurrencyData();
-        } catch (error) {
-            this.logger.error(error);
-            return { error: error.message };
-        }
+  @Get('exchange-rates')
+  async getCurrency(): Promise<any> {
+    try {
+      return await this.currencyService.getCurrencyData();
+    } catch (error) {
+      this.logger.error(error);
+      return { statusCode: error.status, error: error.message };
     }
+  }
 }
