f738d0e208d30821024ccec8675c265effcf6fe9
matheus-arj <matheus.arj@hotmail.com>
2025-03-18 23:37:08 -0300

    feat: added api url exception

--- src/exceptions/enums/exceptions.enum.ts
+++ src/exceptions/enums/exceptions.enum.ts
@@ -2,4 +2,5 @@ export class ExceptionsEnum {
   static readonly EXTERNAL_API_NO_DATA = 'No data returned from the external API';
   static readonly EXTERNAL_API_ERROR =
     'Error occurred while trying to fetch data from the external API';
+  static readonly API_URL_NOT_FOUND = 'API url is not defined';
 }
