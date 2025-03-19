7def56ad50391fbdf930c27f7aa6b65df3fac723
matheus-arj <matheus.arj@hotmail.com>
2025-03-18 23:40:58 -0300

    feat: added env url, created method to filter response data and return the currency dto

--- src/services/currency.service.ts
+++ src/services/currency.service.ts
@@ -1,5 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import axios from 'axios';
+import { CurrencyDto } from 'src/dtos/currency.dto';
+import { ExceptionsEnum } from 'src/exceptions/enums/exceptions.enum';
 import { ExternalApiException, ExternalApiNoDataException } from 'src/exceptions/exceptions';
 
 @Injectable()
@@ -8,34 +11,50 @@ export class CurrencyService {
   private readonly logger = new Logger(CurrencyService.name);
 
   //API URL to get currency exchange data
-  private readonly API_URL = 'https://economia.awesomeapi.com.br/all';
+  private readonly URL = process.env.API_URL;
 
-  async getCurrencyData(): Promise<any> {
+  async getCurrencyData(): Promise<CurrencyDto[]> {
     try {
-      const response = await axios.get(this.API_URL);
+      if (!this.URL) {
+        throw new BadRequestException(ExceptionsEnum.API_URL_NOT_FOUND);
+      }
+
+      //Call to external API
+      const response = await axios.get(this.URL);
 
       //If the data is empty, throw an error
       if (!response.data || Object.keys(response.data).length === 0) {
         throw new ExternalApiNoDataException();
       }
 
-      //Filter the data to get only the necessary data
-      const filteredData = Object.keys(response.data).map((key) => {
-        return {
-          currency: key,
-          name: response.data[key].name,
-          high: response.data[key].high,
-          low: response.data[key].low,
-          createdDate: response.data[key].create_date,
-        };
-      });
-
+      const filteredData = this.filterData(response.data);
       return filteredData;
     } catch (error) {
       if (error instanceof ExternalApiNoDataException) {
         throw error;
       }
+
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+
       throw new ExternalApiException();
     }
+  }
+
+  private filterData(
+    data: Record<string, { name: string; high: string; low: string; create_date: string }>,
+  ): CurrencyDto[] {
+    //Filter the data to get only the necessary data
+    const filteredData = Object.keys(data).map((key) => {
+      return {
+        currency: key,
+        name: data[key].name,
+        high: data[key].high,
+        low: data[key].low,
+        createdDate: data[key].create_date,
+      };
+    });
+    return filteredData;
   }
 }
