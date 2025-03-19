99e57d9d4cc10327940e140fb5bb73b6203ec644
matheus-arj <matheus.arj@hotmail.com>
2025-03-19 00:50:59 -0300

    feat: added dotenv and void to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { CurrencyModule } from './currency.module';
 
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(CurrencyModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
