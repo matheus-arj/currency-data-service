4545bef6efb9ee627f65cbac02c28621ba999cba
matheus-arj <matheus.arj@hotmail.com>
2025-03-19 15:57:42 -0300

    feat: added swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import { CurrencyModule } from './currency.module';
 
@@ -6,6 +7,17 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(CurrencyModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Exchange API')
+    .setDescription('API documentation for querying exchange rates')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
