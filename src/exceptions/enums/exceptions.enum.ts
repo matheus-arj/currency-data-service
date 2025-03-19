b3677ddfd04ab047256b7fc9052a98aa0a03a1cd
matheus-arj <matheus.arj@hotmail.com>
2025-03-19 00:45:04 -0300

    feat: renamed url to uppercase

--- src/exceptions/enums/exceptions.enum.ts
+++ src/exceptions/enums/exceptions.enum.ts
@@ -2,5 +2,5 @@ export class ExceptionsEnum {
   static readonly EXTERNAL_API_NO_DATA = 'No data returned from the external API';
   static readonly EXTERNAL_API_ERROR =
     'Error occurred while trying to fetch data from the external API';
-  static readonly API_URL_NOT_FOUND = 'API url is not defined';
+  static readonly API_URL_NOT_FOUND = 'API URL is not defined';
 }
