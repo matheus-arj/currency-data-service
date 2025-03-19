8e05fa8a666efb211a6474cc3509ac55816ffba4
matheus-arj <matheus.arj@hotmail.com>
2025-03-19 00:36:15 -0300

    feat: removed try catch from controller and logger

--- src/controllers/currency.controller.ts
+++ src/controllers/currency.controller.ts
@@ -1,19 +1,13 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CurrencyDto } from 'src/dtos/currency.dto';
 import { CurrencyService } from '../services/currency.service';
 
 @Controller('currency')
 export class CurrencyController {
-  private readonly logger = new Logger(CurrencyController.name);
-
   constructor(private readonly currencyService: CurrencyService) {}
 
   @Get('exchange-rates')
-  async getCurrency(): Promise<any> {
-    try {
-      return await this.currencyService.getCurrencyData();
-    } catch (error) {
-      this.logger.error(error);
-      return { statusCode: error.status, error: error.message };
-    }
+  async getCurrency(): Promise<CurrencyDto[]> {
+    return await this.currencyService.getCurrencyData();
   }
 }
