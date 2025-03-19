2f317a746e61ece1327729776870746e2d671f0b
matheus-arj <matheus.arj@hotmail.com>
2025-03-18 22:46:15 -0300

    feat: created exceptions enum and exceptions file to handle with specific exceptions

---
+++ src/exceptions/exceptions.enum.ts
@@ -0,0 +1,5 @@
+export class ExceptionsEnum {
+  static readonly EXTERNAL_API_NO_DATA = 'No data returned from the external API';
+  static readonly EXTERNAL_API_ERROR =
+    'Error occurred while trying to fetch data from the external API';
+}
---
+++ src/exceptions/exceptions.ts
@@ -0,0 +1,20 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { ExceptionsEnum } from './exceptions.enum';
+
+/**
+ * Custom exception to handle when no data is returned from the external API.
+ */
+export class ExternalApiNoDataException extends HttpException {
+  constructor() {
+    super(ExceptionsEnum.EXTERNAL_API_NO_DATA, HttpStatus.NOT_FOUND);
+  }
+}
+
+/**
+ * Custom exception to handle when there is an error while trying to get data from the external API.
+ */
+export class ExternalApiException extends HttpException {
+  constructor() {
+    super(ExceptionsEnum.EXTERNAL_API_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
